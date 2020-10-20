7b57cb5bb8803a602fd28f45cc509a9565b4204c
Dhiraj Himani <dhimani@blackberry.com>
2020-10-20 20:25:18 +0530

    Q1_07_Rotate_Matrix - boring

--- src/solutions/Ch01ArraysandStrings/Q1_07_Rotate_Matrix/Question.java
+++ src/solutions/Ch01ArraysandStrings/Q1_07_Rotate_Matrix/Question.java
@@ -9,30 +9,48 @@ public class Question {
 		int n = matrix.length;
 		
 		for (int layer = 0; layer < n / 2; layer++) {
-			int first = layer;
 			int last = n - 1 - layer;
-			for(int i = first; i < last; i++) {
-				int offset = i - first;
-				int top = matrix[first][i]; // save top
+			System.out.println("layer " + layer + " ");
+			System.out.println("last " + last + " ");
+			for(int i = layer; i < last; i++) {
+				int offset = i - layer;
 
+				System.out.println("i " + i + " ");
+				System.out.println("offset  = i - layer = " + offset + " ");
+				System.out.printf("top [%d][%d] " + " ", layer, i);
+				System.out.println();
+				System.out.println();
+				int top = matrix[layer][i]; // save top
+
+
+				System.out.printf("left [%d][%d] " + " ", last-offset, layer);
+				System.out.printf("top [%d][%d] " + " \n", layer, i);
 				// left -> top
-				matrix[first][i] = matrix[last-offset][first]; 			
+				matrix[layer][i] = matrix[last-offset][layer];
 
+				System.out.printf("bottom [%d][%d] " + " ", last, last - offset);
+				System.out.printf("left [%d][%d] " + " \n", last-offset, layer);
 				// bottom -> left
-				matrix[last-offset][first] = matrix[last][last - offset]; 
+				matrix[last-offset][layer] = matrix[last][last - offset];
 
+				System.out.printf("right [%d][%d] " + " ", i, last);
+				System.out.printf("bottom [%d][%d] " + " \n", last, last - offset);
 				// right -> bottom
-				matrix[last][last - offset] = matrix[i][last]; 
+				matrix[last][last - offset] = matrix[i][last];
 
+				System.out.printf("top [%d][%d] " + " ", layer, i);
+				System.out.printf("right [%d][%d] " + " \n", i, last);
 				// top -> right
 				matrix[i][last] = top; // right <- saved top
+
+				System.out.println();
 			}
 		}
 		return true;
 	}
 	
 	public static void main(String[] args) {
-		int[][] matrix = AssortedMethods.randomMatrix(3, 3, 0, 9);
+		int[][] matrix = AssortedMethods.randomMatrix(4, 4, 0, 9);
 		AssortedMethods.printMatrix(matrix);
 		rotate(matrix);
 		System.out.println();
---
+++ src/whiteboardsolutions/Ch01ArraysandStrings/Q7RotateMatrix/RotateMatrix.java
@@ -0,0 +1,8 @@
+package whiteboardsolutions.Ch01ArraysandStrings.Q7RotateMatrix;
+
+public class RotateMatrix {
+
+    public static void rotateMatrix(int[][] matrix) {
+        
+    }
+}
