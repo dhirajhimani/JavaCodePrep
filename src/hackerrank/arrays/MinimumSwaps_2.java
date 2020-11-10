3f19624924636fce6e895e8b7edae9e503407fd9
Dhiraj Himani <dhimani@blackberry.com>
2020-11-10 19:56:32 +0530

    Minimum Swaps 2
    https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays

---
+++ src/hackerrank/arrays/MinimumSwaps_2.java
@@ -0,0 +1,66 @@
+package hackerrank.arrays;
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class MinimumSwaps_2 {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+
+        int swaps = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i] != i + 1) {
+                swap(arr, i, arr[i] - 1);
+                swaps++;
+            }
+        }
+        return swaps;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    // https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+//    7
+//    1 3 5 2 4 6 7
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+}
